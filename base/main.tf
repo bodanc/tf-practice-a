12b1d76f0b365c4f8981a97ea4ec6b3b2f01b956
Bogdan <bogdan@Bogdans-MBP.home>
2025-11-03 13:08:30 +0100

    update main.tf

--- base/main.tf
+++ base/main.tf
@@ -1,7 +1,6 @@
 /*
-This Terraform configuration sets up a basic web application on AWS using an EC2 instance running Nginx.
-It includes the necessary networking components such as a VPC, subnet, internet gateway, and security groups.
-AWS credentials are required to apply this configuration and can be set using environment variables or the AWS CLI.
+this Terraform configuration sets up a basic web application on AWS using an EC2 instance running NGINX;
+it includes the necessary networking components such as a VPC, subnet, internet gateway, and security groups;
 */
 
 terraform {
